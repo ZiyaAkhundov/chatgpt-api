b976ce6884425490872e5ffe9aa262c28b363fec
Ziya Akhundov <95044753+ZiyaAkhundov@users.noreply.github.com>
2023-03-30 22:29:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 import { config } from "dotenv";
 config()
-
 import { Configuration,OpenAIApi } from "openai";
 import readline from "readline";
-
 
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY
 }))
-
 const createUserInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
